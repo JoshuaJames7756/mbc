92b58d30e3bc4d37a92506a54d0a15bdcc419a09
JoshuaJames7756 <joshuajames7756@gmail.com>
2026-08-29 17:11:46 -0400

    gg

--- mbc/src/main.jsx
+++ mbc/src/main.jsx
@@ -1,10 +1,8 @@
-// src/main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ClerkProvider } from '@clerk/clerk-react'
-import { Analytics } from '@vercel/analytics/react'
-import './assets/css/global.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
+import './assets/css/global.css';
+import App from './App.jsx';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -16,7 +14,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
-      <Analytics />
     </ClerkProvider>
   </StrictMode>,
-)
+);
